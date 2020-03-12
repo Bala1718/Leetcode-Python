f7c49ac5e78f641a6fa3c97e5d1745ddc561f3ed
Balaji Mj <mjbalaji17@gmail.com>
2020-03-12 13:59:32 +0530

    Update Min-Depth.py

--- Trees/Min-Depth.py
+++ Trees/Min-Depth.py
@@ -1,15 +1,15 @@
 class Solution:
-    def minDepth(self,root):
+    def min_Depth(self,root):
         if not root:
             return 0
         if not root.left and not root.right:
             return 1
         if not root.left:
-            return self.minDepth(root.right)+1
+            return self.min_Depth(root.right)+1
         if not root.right:
-            return self.minDepth(root.left)+1
+            return self.min_Depth(root.left)+1
         
-        min_left = self.minDepth(root.left)
-        min_right = self.minDepth(root.right)
-        min_depth = min(min_left,min_right) + 1
-        return min_depth
+        m_left = self.min_Depth(root.left)
+        m_right = self.min_Depth(root.right)
+        m_depth = min(m_left,m_right) + 1
+        return m_depth
