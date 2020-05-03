fb18640ada732b9fe688e0ed5ddfc3a55f09ee48
Balaji Mj <mjbalaji17@gmail.com>
2020-05-03 16:39:12 +0530

    Update reverselinkedlist.py

--- LinkedList/reverselinkedlist.py
+++ LinkedList/reverselinkedlist.py
@@ -1,1 +1,16 @@
-
+def reverseList(self, head):
+    if (head == None):
+        return None
+    if( head.next == None):
+        return head
+    ret = head
+    tmp = head
+    
+    head = head.next
+    ret.next = None
+    while (head!=None):
+        tmp = head
+        head = head.next
+        tmp.next = ret
+        ret = tmp
+    return ret
