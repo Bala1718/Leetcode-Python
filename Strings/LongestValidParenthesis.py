1cc5c8f4f1e7d2e599886fc564a1c2c0595ea263
Balaji Mj <mjbalaji17@gmail.com>
2020-05-06 23:18:27 +0530

    Update LongestValidParenthesis.py

--- Strings/LongestValidParenthesis.py
+++ Strings/LongestValidParenthesis.py
@@ -1,17 +1,13 @@
-class Solution:
-    def longestValidParentheses(self, s):
-        stack = [0]
-        longest = 0
-        
-        for c in s:
-            if c == "(":
-                stack.append(0)
-            else:
-                if len(stack) > 1:
-                    val = stack.pop()
-                    stack[-1] += val + 2
-                    longest = max(longest, stack[-1])
-                else:
-                    stack = [0]
-
-        return longest
+def longestValidParentheses(self, s):
+    if not s: 
+        return 0
+    #position represents the starting point of LVP ending at i
+    # set the position[i] = i+1 to avoid the corner case
+    position, result = range(1, len(s)+1), 0
+    
+    for i in range(1, len(s)):
+        j = position[i-1] - 1
+        if s[i] == ")" and j >= 0 and s[j] == "(":
+            position[i] = j if position[j-1] > j-1 else position[j-1]
+            result = max(result, i - position[i] + 1)
+    return result
